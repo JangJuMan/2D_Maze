684abaaebdc9b65b2812cf620b60297ec47d5f6f
JangJuMan <wnaks9901@gmail.com>
2024-11-04 17:47:29 +0900

    [Fix] 게임 정지중엔 미로를 움직이지 못하게 함
    1. Time.deltaTime 이 0 이면 미로 움직임 없도록 조건추가
    2. 기존 FixedUpdate --> Update
     - 미로 움직임을 더 부드럽게 조절

--- Assets/Scripts/MazeMoveHandler.cs
+++ Assets/Scripts/MazeMoveHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MazeMove : MonoBehaviour
 {
-    private float mouse_speed = 2.0f;
+    private float mouse_speed = 1.0f;
     private float keyboard_speed = 0.4f;
 
     void Awake(){
@@ -19,18 +19,19 @@ public class MazeMove : MonoBehaviour
         // Application.targetFrameRate = 60;
     }
 
-    void FixedUpdate(){
-        if(Input.GetMouseButton(0)){
-            transform.Rotate(0f, 0f, -Input.GetAxis("Mouse X") * mouse_speed, Space.World);
-            transform.Rotate(0f, 0f, Input.GetAxis("Mouse Y") * mouse_speed, Space.World);
-        }
-
-        if(Input.GetButton("Horizontal")){
-            transform.Rotate(0f, 0f, Input.GetAxis("Horizontal") * keyboard_speed, Space.World);
+    void Update(){
+        if(Time.deltaTime != 0){
+            if(Input.GetMouseButton(0)){
+                transform.Rotate(0f, 0f, -Input.GetAxis("Mouse X") * mouse_speed, Space.World);
+                transform.Rotate(0f, 0f, Input.GetAxis("Mouse Y") * mouse_speed, Space.World);
+            }
+            if(Input.GetButton("Horizontal")){
+                transform.Rotate(0f, 0f, Input.GetAxis("Horizontal") * keyboard_speed, Space.World);
+            }
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
         
     }
