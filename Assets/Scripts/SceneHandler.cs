792631fc82c229be9a8792e685f6732396424204
JangJuMan <wnaks9901@gmail.com>
2025-02-19 16:07:09 +0900

    [Feat] Level 4_3 제작
    기존 Level 4_2를 4_3으로 변경
    해당 커밋에서 만든 Level 4_3을 4-2로 변경(십자맵)

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -8,7 +8,7 @@ public class SceneHandler : MonoBehaviour
     void Awake(){
         Application.targetFrameRate = 120;
         //FOR DEBUG
-        PlayerPrefs.SetInt("currStage", 11);
+        PlayerPrefs.SetInt("currStage", 12);
         PlayerPrefs.SetInt("userHintCnt", 12);
     }
 
@@ -22,7 +22,7 @@ public class SceneHandler : MonoBehaviour
                     break;
                 case "CharacterScene" :
                 case "Stages_solo" :
-                case "InGame_solo" :
+                case "InGame_solo" :  // 현재 사용하지 않는 페이지
                 case "Settings":
                 case "Explanation":
                     LoadMainScene();
